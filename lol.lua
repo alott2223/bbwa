24333dd4c945069d3f9db26d22714c87ad1887f5
alott2223 <mcdonaldsincorperated@gmail.com>
2026-06-27 22:25:21 -0700

    Refactor lol.lua to improve execution handling
    Reworked the script to utilize new libraries and added executor checks.

--- lol.lua
+++ lol.lua
@@ -1,3 +1,41 @@
+local source = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alott2223/bbwa/refs/heads/main/lol.lua"))()
+]]
+
+local executor = string.lower(identifyexecutor and identifyexecutor() or "")
+local threadSource = [[
+    for _, func in getgc(false) do
+        if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+            ]] .. source .. [[
+            break
+        end
+    end
+]]
+
+local function runSource(runner, getAll)
+    for _, actor in getAll() do
+        runner(actor, threadSource)
+    end
+end
+
+if (string.find(executor, "wave") or string.find(executor, "choco")) then
+    runSource(run_on_actor, get_deleted_actors)
+elseif string.find(executor, "volt") then
+    runSource(run_on_actor, getactors)
+elseif string.find(executor, "potassium") then
+    runSource(run_on_thread, getactorthreads)
+elseif getfflag and (string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))) == "true") then
+    loadstring(source)()
+elseif setfflag then
+    setfflag("DebugRunParallelLuaOnMainThread", "True")
+
+    if queue_on_teleport then
+        queue_on_teleport(source)
+    end
+    
+    game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
 --[[
 	Hi, this is WholeCream further known as NiceCream
 	I've decided to completely rework it to use some of my libraries.
@@ -235,6 +273,7 @@ do
 	end
 
 	game:GetService("RunService"):UnbindFromRenderStep("FW0a9kf0w2of00-Last")
+	local printconsole = printconsole or rconsoleprint or function(text) print(text) end
 	game:GetService("RunService"):BindToRenderStep("FW0a9kf0w2of00-Last", Enum.RenderPriority.Last.Value, function(...)
 		for i=1, #scheduler do
 			local v = scheduler[i]
